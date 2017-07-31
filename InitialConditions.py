a70bf851e8a723d4312debb7a9b2463a88662a35
Isaac-a95 <isaac.alonso.a.95@gmail.com>
2017-07-31 20:23:58 +0100

    IA: Missing modified file

--- InitialConditions.py
+++ InitialConditions.py
@@ -23,136 +23,136 @@ print 'Loading InitialConditions..'
 
 
 def IsothermalEq(args):
-  p0 = args[0]
-  rho0 = args[1]
+   p0 = args[0]
+   rho0 = args[1]
   
-  P0 = p0*np.exp(-Grid.z/1.)
-  var.rho = rho0*np.exp(-Grid.z/1.)
-  var.momentum = var.momentum*0.
-  var.energy = P0/(par.gamma-1.)
+   P0 = p0*np.exp(-Grid.z/1.)
+   var.rho = rho0*np.exp(-Grid.z/1.)
+   var.momentum = var.momentum*0.
+   var.energy = P0/(par.gamma-1.)
 
 
 def ConstantFlow(args):
-  T0 = args[0]
-  rho0 = args[1]
-  v = args[1]
+   T0 = args[0]
+   rho0 = args[1]
+   v = args[1]
 
-  var.rho = rho0*np.ones(Grid.z.shape)
-  var.momentum = var.rho*v
-  var.energy = par.cv*T0*np.ones(Grid.z.shape) + 0.5*var.rho*v*v
+   var.rho = rho0*np.ones(Grid.z.shape)
+   var.momentum = var.rho*v
+   var.energy = par.cv*T0*np.ones(Grid.z.shape) + 0.5*var.rho*v*v
 
 def SoundWaves(args):
-  rho0 = args[0]
-  A = args[1]
-  p0 = args[2]
-  Nwav = args[3]
+   rho0 = args[0]
+   A = args[1]
+   p0 = args[2]
+   Nwav = args[3]
 
-  #K = Nwav*2*np.pi/(Grid.zf-Grid.z0)
-  phas = Nwav * 2. * np.pi *(Grid.z-par.z0)/(par.zf-par.z0)
+   #K = Nwav*2*np.pi/(Grid.zf-Grid.z0)
+   phas = Nwav * 2. * np.pi *(Grid.z-par.z0)/(par.zf-par.z0)
   
-  var.rho = rho0*(1. + A*np.cos(phas) )
+   var.rho = rho0*(1. + A*np.cos(phas) )
   
-  c_s = np.sqrt(par.gamma*p0/rho0)
-  vIn =  c_s*A*np.cos(phas)
-  var.momentum = vIn*var.rho
+   c_s = np.sqrt(par.gamma*p0/rho0)
+   vIn =  c_s*A*np.cos(phas)
+   var.momentum = vIn*var.rho
 
-  pIn = p0*(1. + par.gamma*A*np.cos(phas) )
-  var.energy = pIn/(par.gamma-1.) + 0.5*var.rho*vIn*vIn
+   pIn = p0*(1. + par.gamma*A*np.cos(phas) )
+   var.energy = pIn/(par.gamma-1.) + 0.5*var.rho*vIn*vIn
 
 
 def LogTLogRhoProfile(args):
-  logTA = np.log(args[0])
-  logTB = np.log(args[1])
-  rhoA = args[2]
-  rhoB = args[3]
+   logTA = np.log(args[0])
+   logTB = np.log(args[1])
+   rhoA = args[2]
+   rhoB = args[3]
 
-  par.g = -27360.00
-  par.R = 8.3144598e+07
-  par.mu = 1.113202
+   par.g = -27360.00
+   par.R = 8.3144598e+07
+   par.mu = 1.113202
 
-  logrho = np.log(rhoA) + (np.log(rhoB)-np.log(rhoA))/(Grid.z[-1]-Grid.z[0]) * (Grid.z-Grid.z[0])
+   logrho = np.log(rhoA) + (np.log(rhoB)-np.log(rhoA))/(Grid.z[-1]-Grid.z[0]) * (Grid.z-Grid.z[0])
 
-  logT = logTA + (logTB-logTA)/(Grid.z[-1]-Grid.z[0]) * (Grid.z-Grid.z[0])
-  var.T = np.exp(logT)
+   logT = logTA + (logTB-logTA)/(Grid.z[-1]-Grid.z[0]) * (Grid.z-Grid.z[0])
+   var.T = np.exp(logT)
 
   
-  """ 
-  logp1 = np.zeros(var.P.shape)
-  logp1[0] = logpA
+   """ 
+   logp1 = np.zeros(var.P.shape)
+   logp1[0] = logpA
+  
+   T_num = np.zeros(Grid.z.shape)
+   T_num[0] = args[0]
+
+   B = par.mu*par.g/par.R
+   A = par.ct
+   Lambda = 0.
  
-  T_num = np.zeros(Grid.z.shape)
-  T_num[0] = args[0]
+   i=1
+   while i<len(Grid.z):
+      T7_2 = Grid.dz* Grid.dz * 7*Lambda/(2.*A) - T_num[i-2]**(7./2.) + 2*T_num[i-1]**(7./2.)
+      T_num[i] = T7_2**(2./7.)
 
-  B = par.mu*par.g/par.R
-  A = par.ct
-  Lambda = 0.
+      logp[i] = logp[i-1] + Grid.dz*B/var.T[i]   
+      logp1[i] = logp1[i-1] + Grid.dz*B/T_num[i]
 
-  i=1
-  while i<len(Grid.z):
-    T7_2 = Grid.dz* Grid.dz * 7*Lambda/(2.*A) - T_num[i-2]**(7./2.) + 2*T_num[i-1]**(7./2.)
-    T_num[i] = T7_2**(2./7.)
+      i+=1  
+   """
 
-    logp[i] = logp[i-1] + Grid.dz*B/var.T[i]   
-    logp1[i] = logp1[i-1] + Grid.dz*B/T_num[i]
-
-    i+=1  
-  """
-
-  par.Computecv()
-  var.momentum = var.momentum*0.
-  var.rho = np.exp(logrho)
-  var.energy = var.rho*par.cv*var.T
+   par.Computecv()
+   var.momentum = var.momentum*0.
+   var.rho = np.exp(logrho)
+   var.energy = var.rho*par.cv*var.T
 
 
 def LogTGravityProfile(args):
-  logTA = np.log(args[0])
-  logTB = np.log(args[1])
-  rhoA = args[2]
+   logTA = np.log(args[0])
+   logTB = np.log(args[1])
+   rhoA = args[2]
 
-  par.g = -27360.00
-  par.R = 8.3144598e+07
-  par.mu = 1.113202
+   par.g = -27360.00
+   par.R = 8.3144598e+07
+   par.mu = 1.113202
 
-  logT = logTA + (logTB-logTA)/(Grid.z[-1]-Grid.z[0]) * (Grid.z-Grid.z[0])
-  var.T = np.exp(logT)
- 
-  logp = np.zeros(Grid.z.shape)
-  logp[0] = np.log(rhoA*par.R*args[0]/par.mu)
+   logT = logTA + (logTB-logTA)/(Grid.z[-1]-Grid.z[0]) * (Grid.z-Grid.z[0])
+   var.T = np.exp(logT)
+  
+   logp = np.zeros(Grid.z.shape)
+   logp[0] = np.log(rhoA*par.R*args[0]/par.mu)
 
-  B = par.mu*par.g/par.R
-  A = par.ct
-  Lambda = 0.
+   B = par.mu*par.g/par.R
+   A = par.ct
+   Lambda = 0.
 
-  i=1
-  # Compute pressure to make it stationary under gravity
-  while i<len(Grid.z):
+   i=1
+   # Compute pressure to make it stationary under gravity
+   while i<len(Grid.z):
 
-    logp[i] = logp[i-1] + Grid.dz*B/var.T[i]   
+      logp[i] = logp[i-1] + Grid.dz*B/var.T[i]   
 
-    i+=1  
+      i+=1  
 
-  print np.exp(logp)
-  par.Computecv()
-  var.rho = np.exp(logp)*par.mu/(par.R * var.T)
-  var.momentum = var.momentum*0.
-  var.energy = var.rho*par.cv*var.T
+   print np.exp(logp)
+   par.Computecv()
+   var.rho = np.exp(logp)*par.mu/(par.R * var.T)
+   var.momentum = var.momentum*0.
+   var.energy = var.rho*par.cv*var.T
 
 
 def GaussianTemperature(args):
-  T0 = args[0]
-  z0 = args[1]
-  width = args[2]
-  rho0 = args[3]
+   T0 = args[0]
+   z0 = args[1]
+   width = args[2]
+   rho0 = args[3]
   
   
-  exp = np.exp(-(Grid.z-z0)*(Grid.z-z0)/(4*width))
-  var.rho = rho0*np.ones(Grid.z.shape)
-  var.momentum = var.momentum*0.
-  var.energy = (1.+T0*exp)*var.rho*par.cv
-  if par.SpitzerDiffusion:
-    var.kappa = par.ct*np.ones(Grid.z.shape)*(1.+T0*exp)**(5./2.)
-  else: 
-    var.kappa = par.ct*np.ones(Grid.z.shape)*exp #np.ones(Grid.z.shape)
+   exp = np.exp(-(Grid.z-z0)*(Grid.z-z0)/(4*width))
+   var.rho = rho0*np.ones(Grid.z.shape)
+   var.momentum = var.momentum*0.
+   var.energy = (1.+T0*exp)*var.rho*par.cv
+   if par.SpitzerDiffusion:
+      var.kappa = par.ct*np.ones(Grid.z.shape)*(1.+T0*exp)**(5./2.)
+   else: 
+      var.kappa = par.ct*np.ones(Grid.z.shape)*exp #np.ones(Grid.z.shape)
   
   
 def ReadICFromFileTemperature(args):
